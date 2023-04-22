a8109075128427c74f9d4144a2b51400e04c7d88
AndrewGalatsan <115672771+AndrewGalatsan@users.noreply.github.com>
2023-04-22 13:10:14 -0400

    new application file

---
+++ src/reducers/application.js
@@ -0,0 +1,27 @@
+export const SET_DAY = "SET_DAY";
+export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+
+
+function getSpotsForDay(state, day) {
+  return state.days
+    .find(d => d.name === day)
+    .appointments.reduce((a, c) => {
+      return state.appointments[c].interview ? a : a + 1;
+    }, 0);
+}
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return {
+        ...state,
+        day: action.day
+      };
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      };
+    
